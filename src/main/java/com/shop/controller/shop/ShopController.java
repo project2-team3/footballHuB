3415d196e6bfc6879f8891015efd929874a8dda2
jiyoon-lee <jiyoon3421@gmail.com>
2023-10-14 00:04:18 +0900

    content가 화면을 꽉차게 디자인 수정

--- src/main/java/com/shop/controller/shop/ShopController.java
+++ src/main/java/com/shop/controller/shop/ShopController.java
@@ -1,6 +1,8 @@
 package com.shop.controller.shop;
+import com.shop.dto.CategoryDto;
 import com.shop.dto.ItemSearchDto;
 import com.shop.dto.MainItemDto;
+import com.shop.service.CategoryService;
 import com.shop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -11,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -19,12 +22,16 @@ import java.util.Optional;
 public class ShopController {
     private final ItemService itemService;
 
+    private final CategoryService categoryService;
+
     @GetMapping
     public String shopMain(ItemSearchDto itemSearchDto, Optional<Integer> page, Model model){
 
         Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 6);
         Page<MainItemDto> items = itemService.getMainItemPage(itemSearchDto, pageable);
 
+        List<CategoryDto> categoryDtoList =  categoryService.getAllCategory();
+        model.addAttribute("categoryDtoList", categoryDtoList);
         model.addAttribute("items", items);
         model.addAttribute("itemSearchDto", itemSearchDto);
         model.addAttribute("maxPage", 5);
