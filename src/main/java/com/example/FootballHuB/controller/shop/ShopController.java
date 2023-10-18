e898acaa927c0602174ecb8e41095b22bd542d65
jiyoon-lee <jiyoon3421@gmail.com>
2023-10-19 02:24:44 +0900

    Header 오동작 수정

--- src/main/java/com/example/FootballHuB/controller/shop/ShopController.java
+++ src/main/java/com/example/FootballHuB/controller/shop/ShopController.java
@@ -2,6 +2,11 @@ package com.example.FootballHuB.controller.shop;
 import com.example.FootballHuB.dto.CategoryDto;
 import com.example.FootballHuB.dto.ItemSearchDto;
 import com.example.FootballHuB.dto.MainItemDto;
+import com.example.FootballHuB.entity.Member;
+import com.example.FootballHuB.entity.Order;
+import com.example.FootballHuB.repository.CartRepository;
+import com.example.FootballHuB.repository.MemberRepository;
+import com.example.FootballHuB.repository.OrderRepository;
 import com.example.FootballHuB.service.CategoryService;
 import com.example.FootballHuB.service.ItemService;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +18,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,13 +30,31 @@ public class ShopController {
 
     private final CategoryService categoryService;
 
+    private final CartRepository cartRepository;
+
+    private final MemberRepository memberRepository;
+
+    private final OrderRepository orderRepository;
+
     @GetMapping
-    public String shopMain(ItemSearchDto itemSearchDto, Optional<Integer> page, Model model){
+    public String shopMain(ItemSearchDto itemSearchDto, Optional<Integer> page, Model model, Principal principal){
 
         Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 6);
         Page<MainItemDto> items = itemService.getMainItemPage(itemSearchDto, pageable);
 
         List<CategoryDto> categoryDtoList =  categoryService.getAllCategory();
+        long cartCount = 0;
+        long orderCount = 0;
+        if(principal != null) {
+            Member member = memberRepository.findByEmail(principal.getName());
+            cartCount = cartRepository.countByMemberId(member.getId());
+
+            orderCount = orderRepository.countByMemberId(member.getId());
+            System.out.println(cartCount);
+        }
+
+        model.addAttribute("cartCount", cartCount);
+        model.addAttribute("orderCount", orderCount);
         model.addAttribute("categoryDtoList", categoryDtoList);
         model.addAttribute("items", items);
         model.addAttribute("itemSearchDto", itemSearchDto);
