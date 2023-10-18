4a2f842179ec284e93c9812c1813cf90365151bf
I3andal <81844942+Bandal1116@users.noreply.github.com>
2023-10-18 11:09:46 +0900

    Update MainController.java

--- src/main/java/com/example/FootballHuB/controller/MainController.java
+++ src/main/java/com/example/FootballHuB/controller/MainController.java
@@ -1,19 +1,1 @@
-package com.example.FootballHuB.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.example.FootballHuB.service.ItemService;
-import lombok.RequiredArgsConstructor;
-@Controller
-@RequiredArgsConstructor
-public class MainController {
-
-    private final ItemService itemService;
-
-    @GetMapping(value = "/")
-    public String mainPage(){
-        return "main";
-    }
-
-}
